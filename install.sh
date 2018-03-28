bdbe7b3c4ab31568de88877b69567e10d7899ba0
kcs4u4@gmail.com <srinivassalloori@172.16.0.55>
2018-03-28 13:10:44 +0530

    SFDX format commit From AutoRABIT.

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sfdx force:org:create -s -a usertest -f config/project-scratch-def.json
-sfdx force:source:push
-sfdx force:user:permset:assign -n dreamhouse
-sfdx force:data:tree:import -p assets/data/Broker__c-Property__c-plan.json
